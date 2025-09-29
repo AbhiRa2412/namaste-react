a833cfc3705c8801e31d66b2bafef178201bf919
AbhiRa <Abhinavdubey33@yahoo.in>
2025-09-29 16:53:28 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,17 @@
+const heading = React.createElement(
+  "div",
+  { id: "Parent" },
+  [React.createElement(
+    "div",
+    { id: "Child1" },
+    [React.createElement("h1", {}, "Hello h1 from React"),React.createElement("h2", {}, "Hello h2 from React")]
+  ),React.createElement(
+    "div",
+    { id: "Child2" },
+    [React.createElement("h1", {}, "Hello h1 from React"),React.createElement("h2", {}, "Hello h2 from React")]
+  )]
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(heading)
